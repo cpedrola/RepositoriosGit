5a85645022f105ed2e91edc4bddb729d6c433c0a
Carles <cpedrola@ieslavereda.es>
2021-02-26 22:58:49 +0100

    Cambio en principal con cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
  */
 	public static void main(String[] args) {
 		System.out.println("Hola mundo por tercera vez");
+		System.out.println("Pruebas Git. Se añade Cambio 1");
 	}
 	
 }
